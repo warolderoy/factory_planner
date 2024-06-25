9935566159d2c2ee9845b74362e1d4366917275c
warolderoy <vincentdrolet@msn.com>
2024-06-25 16:12:26 -0400

    Added window

---
+++ src/graphics.py
@@ -0,0 +1,37 @@
+from tkinter import Tk, BOTH, Canvas
+# Remember to use python 3.10 to call
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Factory Planner")
+        self.__canvas = Canvas(
+            master=self.__root,
+            bg="#373737",
+            height=height,
+            width=width
+        )
+        self.__canvas.pack(
+            fill=BOTH,
+            expand=1
+        )
+        self.__running = False
+
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        
+        while self.__running:
+            self.redraw()
+        print("Window closed successfully")
+    
+    def close(self):
+        self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
---
+++ src/main.py
@@ -0,0 +1,11 @@
+from graphics import Window
+
+def main():
+    screen_x = 800
+    screen_y = 600
+
+    win = Window(screen_x, screen_y)
+
+    win.wait_for_close()
+
+main()
