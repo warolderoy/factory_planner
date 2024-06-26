87039cbc086de7752a96019165841aaae0ea8219
warolderoy <vincentdrolet@msn.com>
2024-06-26 13:39:10 -0400

    Added tests

--- src/tests.py
+++ src/tests.py
@@ -1,7 +1,16 @@
 import unittest
 
+from item import Item
+
 class Tests(unittest.TestCase):
-    pass
+    # Item tests
+    def test_item_no_fuel(self):
+        item = Item("Copper plate")
+        self.assertEqual(item.__repr__(), "Item(Copper plate, None)")
+    
+    def test_item_burnable(self):
+        item = Item("Coal", 4)
+        self.assertEqual(item.burnable(), True)
 
 if __name__ == "__main__":
     unittest.main()
