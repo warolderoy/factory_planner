89ee18b0158c276ac97df9b4f65ef4f0920f99b9
warolderoy <vincentdrolet@msn.com>
2024-08-03 14:45:08 -0400

    Push commit

--- src/block.py
+++ src/block.py
@@ -1,9 +1,11 @@
 class Block:
-    def __init__(self, name):
+    def __init__(self, name, main_output, output_amount=0):
         self.name = name
         self.__rm_pairs = []
         self.__item_inputs = {}
         self.__item_outputs = {}
+        self.main_output = main_output
+        self.output_amount = output_amount
 
     def add_rm_pair(self, recipe, machine):
         # raise Exception("This recipe is already used")
@@ -19,6 +21,10 @@ class Block:
         self.__item_inputs = {}
         self.__item_outputs = {}    
         # Check if an item is both in inputs and outputs
+        # TODO: Equilibrate scales of rm_pairs to not have items both in input and output
+        # Search through rm_pairs to see which one is the "main rm_pair" (or have it be set at the start)
+        # Once you have the main pair, scale it so that the main output is the same as output_amount
+        # Now search through the other rm_pairs to see if one of their outputs is in the inputs of the main pair
 
 
     def get_recipe_machine_pairs(self):
